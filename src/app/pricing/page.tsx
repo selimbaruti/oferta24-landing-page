d30588b2af98c84b0ed6ef985a7eee87d59a3cc7
Dan Billson <daniel.billson@paddle.com>
2025-06-10 11:37:29 +0100

    fix: replace blur svgs with images (#21)

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -4,8 +4,8 @@ import { RadialBlur } from "@/components/pricing/radial-blur";
 
 export default function Pricing() {
   return (
-    <div className="bg-card flex h-full min-h-screen w-full flex-col p-8">
-      <RadialBlur className="-top-40 z-0 md:-top-0" />
+    <div className="bg-card isolate flex h-full min-h-screen w-full flex-col p-8">
+      <RadialBlur />
       <Nav />
       <Plans />
     </div>
